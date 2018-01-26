07bfa976849001324e9aa3b663d2578823b3f045
Fernán González Domingo <fernangonzalez@gmail.com>
2018-01-26 15:53:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-function(musicians, instruments) {
-  var x[]
+function theBeatlesPlay(musicians, instruments) {
+  const array = []
+
+  for (var i = 0, l = musicians.length; i < l; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+
+  return array
 }
