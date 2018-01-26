3c629331a65b7672f4658220c1dae684dd093879
Fernán González Domingo <fernangonzalez@gmail.com>
2018-01-26 16:05:03 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function theBeatlesPlay(musicians, instruments) {
+  const array = []
+
+  for (var i = 0, l = musicians.length; i < l; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+
+  return array
+}
+
+function johnLennonFacts(facts) {
+  const factsWithExclamationMarks = []
+
+  var i = 0
+  while (i < facts.length) {
+    factsWithExclamationMarks.push(`${facts[i]}!!!`)
+    i++
+  }
+
+  return factsWithExclamationMarks
+}
+
+function iLoveTheBeatles(number){
+  const array=[]
+  do {
+    array.push('I love the Beatles!')
+    number++
+    } while (number<15)
+    
+  return array
+}
